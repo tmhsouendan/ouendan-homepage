44947aed742436505df5ddf274b3269c57ba246d
tmhsouendan <tmhs.ouendan@gmail.com>
2026-08-07 14:21:55 +0900

    update

--- site.js
+++ site.js
@@ -5,7 +5,7 @@ let current = 0;
 setInterval(() => {
     slides[current].classList.remove("active");
 
-    current = (current + 1) % slides.lengsth;
+    current = (current + 1) % slides.length;
 
     slides[current].classList.add("active");
 }, 4000);
